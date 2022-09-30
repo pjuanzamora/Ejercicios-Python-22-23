5f48f0e28ace233c4906329a51528d35ab923cc4
Juan Francisco <pjuanzamora@gmail.com>
2022-09-30 11:16:36 +0200

    Sentencias condicionales y casting de datos

---
+++ Sentencias Condicionales.py
@@ -0,0 +1,12 @@
+#Sentencias condicionales
+
+#1-Leer tu edad por teclado e imprima por pantalla si eres mayor de edad o menor
+
+edad = 0
+
+edad = (int) (input("Dime tu edad: "))
+
+if (edad>=18):
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
