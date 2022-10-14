9f46971cad84dd610dc1df21821c0902211e03f3
Juan Francisco <pjuanzamora@gmail.com>
2022-10-14 10:39:50 +0200

    Ejercicio 5 secuenciales termiando

---
+++ Ejercicio5-Secuenciales.py
@@ -0,0 +1,20 @@
+from pathlib import PureWindowsPath
+
+
+usuSecreto = "juan"
+passSecreto = "12"
+
+usuario = input("Dime tu ususario: ")
+password = input("Dime tu contraseña: ")
+
+while (usuSecreto!=usuario or passSecreto!=password):
+    if (usuSecreto!=usuario):
+        print("Error en el usuario")
+        usuario = input("Dime tu ususario: ")
+    elif (passSecreto!=password):
+        print("Error en el password")
+        password = input("Dime tu password: ")
+    
+
+
+print("Usuario y password correcto")
