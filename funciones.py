2934a7de39075ede85451c8b1550c063ba3efca5
Juan Francisco <pjuanzamora@gmail.com>
2022-10-21 11:13:44 +0200

    Ejercicios funciones, area, perimetro

--- funciones.py
+++ funciones.py
@@ -19,4 +19,29 @@ def descargaCancion(url:str):
     cancion.download()
 
 
-descargaCancion("https://www.youtube.com/watch?v=phgH6Pobvw8")
+#Calcular Area y Perimetro
+
+def calcularArea(lado1:int,lado2:int):
+    area = lado1*lado2
+    return area
+
+def calcularPerimetro(lado1:int,lado2:int):
+    perimetro = (2*lado1 + 2*lado2)
+    return perimetro
+
+#Devuelvo una lista con [area, perimetro]
+def calcularAreaYperimetro(lado1:int,lado2:int):
+    vDatos=[]
+    area = calcularArea(lado1,lado2)
+    perimetro= calcularPerimetro(lado1,lado2)
+    vDatos.append(area)
+    vDatos.append(perimetro)
+    return vDatos
+
+#Principal
+lado1 = int(input("Dime el valor del lado1"))
+lado2 = int(input("Dime el valor del lado2"))
+
+vNum = calcularAreaYperimetro(lado1,lado2)
+print("El area es:", vNum[0])
+print("El perimetro es:", vNum[1])
