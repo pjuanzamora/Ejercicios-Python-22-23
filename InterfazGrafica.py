dcc6571bc3908eab000f61817f469eb43ac9e08c
Juan Francisco <pjuanzamora@gmail.com>
2022-11-21 11:16:15 +0100

    Componentes Label, Entry con StringVar

--- InterfazGrafica.py
+++ InterfazGrafica.py
@@ -1,25 +1,42 @@
 from tkinter import *
 from tkinter import ttk
 
+def saludar():
+    texto = campoTexto.get()
+    textoLabel.set(texto)
+
+
 #Generar la ventan
 ventana = Tk()
 ventana.title("Primer ejercicio")
 ventana.geometry("250x300")
 ventana.resizable(width=False,height=False)
 ventana.config(background="blue")
-ventana.iconbitmap("icono.ico")
+
 
 
 #Genera el lienzo para pintar componentes
-frm = ttk.Frame(ventana).pack()
+frm = ttk.Frame(ventana)
+frm.pack()
 
 
 #Componentes Label y Button
-labelTexto = ttk.Label(frm, text="Hola Tkinter")
+textoLabel = StringVar()
+textoLabel.set("Hola Tkinter")
+labelTexto = ttk.Label(frm, textvariable=textoLabel)
 labelTexto.config(background="red", border=5, font=("Arial",15))
 labelTexto.pack()
 
+#Componente cuadro de texto
+campoTexto = ttk.Entry(frm)
+campoTexto.pack()
+
+
+
+ttk.Button(frm, text="Saludo", command=saludar).pack()
 ttk.Button(frm, text="Salir", command=ventana.destroy).pack()
+
+
 
 
 ventana.mainloop()
